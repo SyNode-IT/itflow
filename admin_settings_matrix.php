e78bfc2121670cab6f65da01fdb8324b41767d36
SyNode <97973228+SyNode-IT@users.noreply.github.com>
2024-10-14 19:34:05 +0200

    Update admin_settings_matrix.php

--- admin_settings_matrix.php
+++ admin_settings_matrix.php
@@ -1,4 +1,8 @@
 <?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 require_once "inc_all_admin.php"; // Inclusion globale
 
 // Vérifier si l'administrateur met à jour les paramètres Matrix
